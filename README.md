e957b39d021306a80a99c116a17068ac7aa3b9a7
marvelpokemaster <marvelpokemaster@gmail.com>
2025-09-02 13:39:08 +0530

    update

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ironmon1
 Mon of iron
+pip install dotenv , langchain_groq ,langgraph ,langchain_core, langchain
 
 Basic project using  groq llm
 
--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langgraph.prebuilt import create_react_agent
 from langchain_core.messages import HumanMessage
+from langchain.tools import tool
 
 load_dotenv()
+
+@tool
+def calculator(a: int ,b: int) -> str:
+    """doing basic arithmetic operations like sum on numbers"""
+    # print(f"calc done on {a} and {b}")
+    return f"{a} + {b} = {a+b}"
+
+@tool
+def greet(a: str) -> str:
+    """used for greeting a user"""
+    return "hey dude, I'm your AI!"
+
 def main():
     model = ChatGroq(
     model="deepseek-r1-distill-llama-70b",
@@ -12,7 +25,7 @@ def main():
     reasoning_format="parsed",
     timeout=None,
     max_retries=2)
-    tools=[]
+    tools=[calculator,greet]
     agent_executor=create_react_agent(model,tools)
     print("Hey I'm your AI bot ,can help you anytime!")
     while True:
